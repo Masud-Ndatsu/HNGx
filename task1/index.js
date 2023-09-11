06e2ddb9dd7a63f7342536b2d0b1adfcb1b8d0fe
Masud-Ndatsu <masudndatsu@gmail.com>
2023-09-11 01:19:41 +0100

    added a new resource

--- task1/index.js
+++ task1/index.js
@@ -8,24 +8,46 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  const date = new Date();
+app.get("/api", (req, res) => {
+  try {
+    const date = new Date();
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-  // if() {
+    if (!req.query.slack_name && !req.query.track) {
+      if (
+        req.query.slack_name !== "Biggie.ts" ||
+        req.query.track !== "backend"
+      ) {
+        return res.status(200).json({
+          error_code: 400,
+          message: "Invalid slack name or track provided",
+        });
+      }
+    }
 
-  // }
+    const response = {
+      slack_name: "Biggie.ts",
+      current_day: days[date.getDay()],
+      utc_time: date,
+      track: "backend",
+      github_file_url:
+        "https://github.com/Masud-Ndatsu/HNGx/blob/master/task1/index.js",
+      github_url: "https://github.com/Masud-Ndatsu/HNGx",
+      status_code: 200,
+    };
 
-  const response = {
-    slack_name: "Biggie.ts",
-    current_day: date.getDay(),
-    utc_time: date.getUTCDate(),
-    track: "backend",
-    github_file_url: "",
-    github_url: "",
-    status_code: 200,
-  };
-
-  return res.status(200).json(response);
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log({ error });
+  }
 });
 
 app.listen(port, () => console.log("listening on port: " + port));
